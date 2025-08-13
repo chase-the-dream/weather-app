8d50b4ca7ffa394e9cf0359b9cc743c028a77b17
JuiceChamp <freckmannchase@gmail.com>
2025-08-13 14:05:17 -0400

    Started displaying city info

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 console.log('process.env.WEATHER_API_KEY:', process.env.WEATHER_API_KEY);
 import './styles.css';
+import sunnyIcon from './images/sunny.svg';
 
 const searchBar = document.getElementById('city');
 const submitButton = document.querySelector('.search-button');
+const forecast = document.querySelector('.forecast');
 
 submitButton.addEventListener("click", submitClick);
 searchBar.addEventListener("keydown", handleKeyPress);
 
 function submitClick() {
     const cityName = searchBar.value;
-    alert(`You searched for ${cityName}`);
     getData();
 }
 
@@ -20,7 +21,6 @@ function handleKeyPress(event) {
 }
 
 function getData() {
-    console.log("WHAT IS GOING ON RIGHT NOW");
     const API_KEY = process.env.WEATHER_API_KEY;
 
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchBar.value}?key=${API_KEY}`, {mode: 'cors'})
@@ -28,7 +28,13 @@ function getData() {
         return response.json();
     })
     .then(function(response){
-        console.log(response);
+        forecast.innerHTML = `
+            <h1>${response.address}</h1>
+            <h1>${response.currentConditions.temp}°F</h1>
+            <img src="${sunnyIcon}" alt="image of sunshine">
+            <h2>${response.description}</h2>
+            <h3>HI: 94°F LO: 90°F</h3>
+        `
     });
 }
 
