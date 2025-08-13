8af6a90c6e4029205267856231c1c04a439c15e8
JuiceChamp <freckmannchase@gmail.com>
2025-08-13 11:02:10 -0400

    started json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './styles.css';
+
+const searchBar = document.getElementById('city');
+const submitButton = document.querySelector('.search-button');
+
+submitButton.addEventListener("click", submitClick);
+searchBar.addEventListener("keydown", handleKeyPress);
+
+function submitClick() {
+    const cityName = searchBar.value;
+    alert(`You searched for ${cityName}`);
+    getData();
+}
+
+function handleKeyPress(event) {
+    if (event.key == "Enter") {
+        submitClick();
+    }
+}
+
+function getData() {
+    console.log("WHAT IS GOING ON RIGHT NOW");
+    const API_KEY = "EAQM2BWEDCBGMZGXUPZSULXXL";
+
+    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchBar.value}?key=${API_KEY}`, {mode: 'cors'})
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(response){
+        console.log(response);
+    });
+}
+
